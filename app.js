17b932ac1be32423f33dbdfc99f74d18675f58f0
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-04 10:56:44 -0700

    q2 done

---
+++ app.js
@@ -0,0 +1,24 @@
+//Q1
+ 
+// function power(a,b){
+//  var res=Math.pow(a,b)
+//  console.log(res)
+// }
+// power(2,3)
+
+//Q2
+
+// function checkLeapYear(year) {
+
+//     const leap = new Date(year, 1, 29).getDate() === 29;
+//     if (leap) {
+//         console.log(year + ' is a leap year');
+//     } else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
+
+// // take input
+// const year = prompt('Enter a year:');
+
+// checkLeapYear(year);
